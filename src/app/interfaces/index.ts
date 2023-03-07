2a984418e99945bac23ab15593b42f06bc86a9e8
RFChanchay <rfchanchay@espe.edu.ec>
2023-03-07 13:58:05 -0500

    arreglo feature

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface Index {
+}
+export interface Event{
+    name: string,
+    date: Date,
+    description: string,
+    place: string,
+    price: number,
+    time: string,
+    artist: string,
+    image:string
+}
--- src/app/pages/eventinfo/eventinfo.page.ts
+++ src/app/pages/eventinfo/eventinfo.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-eventinfo',
@@ -6,8 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventinfo.page.scss'],
 })
 export class EventinfoPage implements OnInit {
+  
+ 
+  @Input() name: string;
+  @Input() date: Date;
+  @Input() description: string;
+  @Input() place: string;
+  @Input() price: number;
+  @Input() time: string;
+  @Input() artist: string;
+  @Input() photoEvent:string;
 
-  constructor() { }
+  constructor() { 
+    this.name="",
+    this.date= new Date(1478708162000),
+    this.description="",
+    this.place="",
+    this.price=0,
+    this.time="",
+    this.artist="",
+    this.photoEvent="";
+    this.photoEvent="";
+  }
+  
 
   ngOnInit() {
   }
